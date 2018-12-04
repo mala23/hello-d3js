805f4e359f1b624fc3acf95bd7c0f488a3e6aa2f
Colin Nico Schmid <mail@mala23.me>
2018-12-04 22:15:22 +0100

    draw a bar chart

--- d3-script.js
+++ d3-script.js
@@ -1,7 +1,7 @@
 function d3Script() {
   var dataset = [ 5, 10, 15, 20, 25 ];
 
-  d3.select("body").selectAll("p")
+  d3.select("#exercise01").selectAll("p")
     .data(dataset)
     .enter()
     .append("p")
@@ -12,5 +12,14 @@ function d3Script() {
       } else {
         return "black"
       }
+    })
+  d3.select("#exercise08").selectAll("div")
+    .data(dataset)
+    .enter()
+    .append("div")
+    .attr("class", "bar")
+    .style("height", function(d) {
+      var barHeight = d * 5
+      return barHeight + "px"
     })
 }
