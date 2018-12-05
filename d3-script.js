6c5871b47a3b3866ab3b7262e55a2963b45a2f8e
Colin Nico Schmid <mail@mala23.me>
2018-12-05 14:36:33 +0100

    change dataset from string to variable – finally got that bug

--- d3-script.js
+++ d3-script.js
@@ -42,11 +42,35 @@ function d3Script() {
     })
     .attr("cy", height/2)
     .attr("r", function(d) {
+      console.log(d)
       return d
     })
     .attr("fill", "yellow")
     .attr("stroke", "orange")
     .attr("stroke-width", function(d) {
       return d/2
+    })
+
+  var barsWidth = 500
+  var barsHeight = 100
+  var barsPadding = 1
+
+  var svgBars = d3.select("#exercise13")
+    .append("svg")
+    .attr("width", barsWidth)
+    .attr("height", barsHeight)
+
+  svgBars.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i) {
+      return i * (barsWidth / dataset.length)
+    })
+    .attr("y", 0)
+    .attr("width", barsWidth / dataset.length - barsPadding)
+    .attr("height", function(d) {
+      console.log(d)
+      return d * 4
     })
 }
