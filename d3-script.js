92c1e5a4dba3317b77cc64f6982a66d28337cd8b
Colin Nico Schmid <mail@mala23.me>
2018-12-05 10:30:36 +0100

    draw circles

--- d3-script.js
+++ d3-script.js
@@ -31,4 +31,22 @@ function d3Script() {
     .append("svg")
     .attr("width", width)
     .attr("height", height)
+
+  var circles = svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+
+  circles.attr("cx", function(d, i) {
+    return (i * 50) + 25
+    })
+    .attr("cy", height/2)
+    .attr("r", function(d) {
+      return d
+    })
+    .attr("fill", "yellow")
+    .attr("stroke", "orange")
+    .attr("stroke-width", function(d) {
+      return d/2
+    })
 }
